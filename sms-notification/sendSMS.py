01a2a1ad1d17c4c553953cd5e54d7439fcf908a0
Simon Messerli <Simon.Messerli@amanox.ch>
2019-11-16 02:17:20 +0100

    thank you nico, for this shitty requirements

--- sms-notification/sendSMS.py
+++ sms-notification/sendSMS.py
@@ -5,13 +5,19 @@ def lambda_handler(event, context):
 
     sns = boto3.client('sns')
     
-    print(event)
-    
+    data = json.loads(event['body'])
+
     message = json.loads(event['body'])['message']
-    
-    response = sns.publish(    
-        TopicArn='arn:aws:sns:eu-west-1:829534015160:sns-topic',
-        Message=message,
+
+    if 'number' in data:
+        sns.publish(    
+            PhoneNumber=data['number'],
+            Message=message,
+        ) 
+    else:
+        sns.publish(    
+            TopicArn='arn:aws:sns:eu-west-1:829534015160:sns-topic',
+            Message=message,
         )
     
     return {
